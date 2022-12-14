8aa3423097ac9c174f74ec5f0efc389aa679fa07
Nahuel Leiva <nahuelleiva2002@gmail.com>
2022-12-14 17:30:59 -0300

    Seed data preparado para ser cargado

--- seed_data.py
+++ seed_data.py
@@ -1,7 +1,17 @@
 from appDjango.models import Familiar
+from appDjango.models import Mascota
+from appDjango.models import Vehiculo
 
 Familiar(nombre="Nahuel", fecha_de_nacimiento="02/01/02",dni=43156879).save()
 Familiar(nombre="Matias", fecha_de_nacimiento="20/02/90",dni=43156879).save()
-Familiar(nombre="Martin", fecha_de_nacimiento="21/03/86",dni=43156879).save()
 Familiar(nombre="Viviana", fecha_de_nacimiento="03/07/67",dni=43156879).save()
+
+Mascota(nombre="Rocky", edad="11", animal="Perro")
+Mascota(nombre="Maia", edad="9", animal="Gato")
+Mascota(nombre="Cachy", edad="5", animal="Perro")
+
+Vehiculo(tipo="Auto", antiguedad="5", marca="Renault")
+Vehiculo(tipo="Bicleta", antiguedad="0", marca="Mountainbike")
+Vehiculo(tipo="Moto", antiguedad="1", marca="Yamaha")
+
 print("Se cargo con éxito los usuarios de pruebas")
